1ef9dd75140fb8b860d1baee7e69cb0550877aad
garg-akash <akashgrg2@gmail.com>
2018-08-18 13:50:52 +0530

    Calculate AOI of a given policy using Age at UAV and A at base

--- formulation_version1.m
+++ formulation_version1.m
@@ -8,47 +8,26 @@ vel = 0.5;
 
 %% initial policy
 % 1-base, 2-sensor1, 3-sensor2 .......
-%policy = [1,2,1,3,4,1];
-policy = [1,2,1,3,4,3,2,1];
-% for i = 1:no_poi
-%     for j = 1:length(policy)
-%         if (policy(j) == i)
-%             s(i,j) = 1;
-%         else
-%             s(i,j) = 0;
-%         end
-%     end
-% end
+policy = [1,2,1,3,4,1];
+%policy = [1,2,1,3,4,3,2,1];
+
 s = zeros(length(policy),no_poi)
 for i = 1:length(policy)
     s(i,policy(i)) = 1;
 end
 del_t = calculate_time(poi_coord,policy,vel)
 % A(:,1) is for base which is 0
-A(1,:) = age_init;
+A_uv(1,:) = age_init;
+A_b(1,:) = age_init;
 
 for i = 2:length(policy)
     for j = 2:no_poi
-        A(i,j) = A(i-1,j) + del_t(i);
-        flag1 = 0;
-        flag2 = 0;
-%         for k = 1:i
-%             A(i,j) = A(i,j) - s(i,1)*s(k,j)*A(k,j)
-%         end
-         for k = i:-1:1
-              if(s(i,1) == 1)
-                  if(s(k,j) == 1)
-                    A(i,j) = A(i,j) - A(k,j);
-                    flag2 = flag2 + 1;
-                  end
-                  if(s(k,1) == 1)
-                    flag1 = flag1 + 1;
-                  end
-              end
-              if(flag1 == 2 || flag2 == 1)
-                  break
-              end
-         end
+        A_uv(i,j) = A_uv(i-1,j) + del_t(i);
+        if (s(i,j) == 1)
+            A_uv(i,j) = 0;
+        end
+        A_b(i,j) = (1-s(i,1))*(A_b(i-1,j) + del_t(i)) + s(i,1)*A_uv(i,j);
     end
 end
-   
+
+
